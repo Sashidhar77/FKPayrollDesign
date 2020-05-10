f7ac58a3eb9d018e6fb23ebde510fd658f56cbfd
Sashidhar77 <cs17b039@smail.iitm.ac.in>
2020-05-11 01:35:02 +0530

    Added Union Class and made Class employee extend it In Union Class added methods like joinUnion addSpecialUnionFare getTotalUnionFare Now these methods are automatically extended to other classes if extended

--- project.java
+++ project.java
@@ -20,8 +20,33 @@ class Days {
 	}
 }
 
+class Union extends Days {
+	protected int unionId = -1;
+	protected int unionJoinDate, unionDuePerWeek;
+	protected double totalUnionFare;
 
-class Employee  implements Basic {
+	public void joinUnion(int unId, int dues, int memberShipFee){
+		double oldUnionFare = getTotalUnionFare();
+		unionId = unId;
+		unionDuePerWeek = dues;
+		unionJoinDate = days;
+		totalUnionFare += memberShipFee + oldUnionFare;
+	}
+	public void addSpecialUnionFare(int uid, double toBeAdded){
+		if(unionId == uid)
+		totalUnionFare += toBeAdded;
+	}
+	public double getTotalUnionFare(){
+		if(unionId != -1)
+			totalUnionFare += unionDuePerWeek*(days - unionJoinDate)/7.0;
+		double storeDue = totalUnionFare;
+		totalUnionFare = 0;
+		unionJoinDate = days;
+		return storeDue;
+	}
+}
+
+class Employee extends Union implements Basic {
 	protected int empId, joinDate; 
 	private double salary;
 	protected String modeOfPayment;
@@ -31,6 +56,7 @@ class Employee  implements Basic {
 		joinDate = days;
 		salary = 0;
 		modeOfPayment = "Cash";
+		totalUnionFare = 0;
 	}
 	public int getEmpId(){
 		return empId;
@@ -72,7 +98,7 @@ class DayWager extends Employee {
 	}
 	public void payMoney(){
 		if(days % 7 == 0){
-			System.out.println(empId + " " + (super.getSalary()) + " " + modeOfPayment);
+			System.out.println(empId + " " + (super.getSalary() - super.getTotalUnionFare()) + " " + modeOfPayment);
 			super.setSalaryToZero();
 		}
 	}
